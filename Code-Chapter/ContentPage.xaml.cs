aa3d206cc2aafa699383c2156e811aa8b570e51a
thanhba0 <43293701+thanhba0@users.noreply.github.com>
2019-10-03 22:14:57 +0700

    Add files via upload

---
+++ Code-Chapter/ContentPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+namespace FormsExample
+{
+    public partial class ContentPageExample : ContentPage
+    {
+        public ContentPageExample()
+        {
+            InitializeComponent();
+        }
+        protected void ButtonClicked(object sender, EventArgs e)
+        {
+            ((Button)sender).Text = "It is so!";
+        }
+        private async Task<bool> ButtonClicked(object sender, EventArgs e)
+        {
+            var b = sender as Button;
+            b.Text = "It is so!"; return await Task.FromResult(true);
+        }
+
+    }
+}
